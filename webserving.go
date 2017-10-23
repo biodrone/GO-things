19c8f839a83c952b5d588aead1ef102ebcb6e051
Josh Jacobs <josh@joshjacobs.net>
2017-10-23 21:35:47 +0100

    changing server

--- webserving.go
+++ webserving.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http")
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Dis is da good shit!")
-	fmt.Println("Connection to main page!")
-}
-
-func about_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About da good shit!")
-	fmt.Println("Connection to about page!")
-}
-
-func projects_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About da projects shit!")
-	fmt.Println("Connection to projects page!")
-}
-
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/about", about_handler)
-	http.HandleFunc("/projects", projects_handler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8080", mux)
 }
