b2a40f0e010c5aeb5722361885757398b8fc8241
Josh Jacobs <josh@joshjacobs.net>
2017-10-23 21:12:01 +0100

    added about page

--- webserving.go
+++ webserving.go
@@ -6,10 +6,16 @@ import (
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Dis is da good shit!")
-	fmt.Println("Connection to website!")
+	fmt.Println("Connection to main page!")
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Dis is da good shit!")
+	fmt.Println("Connection to about page!")
 }
 
 func main() {
 	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/about", about_handler)
 	http.ListenAndServe(":8080", nil)
 }
