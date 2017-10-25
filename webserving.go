8e2d67c1e3ea13e2817504ac834fcb96034ee4af
Josh Jacobs <josh@joshjacobs.net>
2017-10-25 22:41:52 +0100

    now works and logs to console displays the IP on /ip but nothing else

--- webserving.go
+++ webserving.go
@@ -5,69 +5,68 @@ import (
 	"net/http"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
-	"github.com/skratchdot/open-golang/open"
 	"log"
 	"strconv"
+	"os"
 )
 
-func getIP(w http.ResponseWriter, req *http.Request, _ httprouter.Params){
-	fmt.Fprintf(w, "<h1>static file server</h1><p><a href='./static'>folder</p></a>")
-
+func getIP(req *http.Request, landing string) (string, string, string, string) {
 	ip, port, err := net.SplitHostPort(req.RemoteAddr)
-	if err != nil {
-		//return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
 
-		fmt.Fprintf(w, "userip: %q is not IP:port", req.RemoteAddr)
+	//check if splitting host and port returned an error
+	if err != nil {
+		log.Fatalf("userip: %q is not IP:port", req.RemoteAddr)
 	}
 
 	userIP := net.ParseIP(ip)
 	if userIP == nil {
-		//return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
-		fmt.Fprintf(w, "userip: %q is not IP:port", req.RemoteAddr)
-		return
+		log.Fatalf("userip: %q is not IP:port", req.RemoteAddr)
+		os.Exit(1)
 	}
 
-	// This will only be defined when site is accessed via non-anonymous proxy
-	// and takes precedence over RemoteAddr
-	// Header.Get is case-insensitive
+	//this will only be defined when site is accessed via non-anonymous proxy
+	//and takes precedence over RemoteAddr
+	//header.Get is case-insensitive
 	forward := req.Header.Get("X-Forwarded-For")
 
-	fmt.Fprintf(w, "<p>IP: %s</p>", ip)
-	fmt.Fprintf(w, "<p>Port: %s</p>", port)
-	fmt.Fprintf(w, "<p>Forwarded for: %s</p>", forward)
+	log.Printf("IP: %s\n", ip)
+	log.Printf("Port: %s\n", port)
+	log.Printf("Forwarded For: %s\n", forward)
+	log.Printf("Visited: %s\n", landing)
+
+	return ip, port, forward, landing
 }
 
-
 func main() {
-	myport := strconv.Itoa(10002);
+	srvPort := strconv.Itoa(3333)
 
-
-	// Instantiate a new router
+	//instantiate a new router
 	r := httprouter.New()
 
-	r.GET("/ip", getIP)
+	//add a handler to echo the IP back to the user
+	r.GET("/ip", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(w, "<h1>Find Your IP</h1>")
+		ip, port, forward, landing := getIP(r, "/ip")
 
-	// Add a handler on /test
-	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		// Simply write some test data for now
-		fmt.Fprint(w, "Welcome!\n")
+		fmt.Fprintf(w, "<p>IP: %s</p>", ip)
+		fmt.Fprintf(w, "<p>Port: %s</p>", port)
+		fmt.Fprintf(w, "<p>Forwarded for: %s</p>", forward)
+		fmt.Fprintf(w, "<p>You Are Visiting: %s</p>", landing)
 	})
 
+	//add a handler on /
+	r.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Simply write some test data for now
+		fmt.Fprint(w, "Welcome!\n")
+		_, _, _, _ = getIP(r, "/")
+	})
 
-	l, err := net.Listen("tcp", "0.0.0.0:" + myport)
+	l, err := net.Listen("tcp", "0.0.0.0:" + srvPort)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// The browser can connect now because the listening socket is open.
 
-
-	//err = open.Start("http://localhost:"+ myport + "/test")
-	err = open.Start("http://localhost:"+ myport + "/ip")
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Start the blocking server loop.
+	//start the blocking server loop.
 	log.Fatal(http.Serve(l, r))
 	//mux := http.NewServeMux()
 	//mux.Handle("/", http.FileServer(http.Dir("./static")))
